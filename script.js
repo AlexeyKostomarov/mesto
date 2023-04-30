788dfe8dcb332f221bc2fc7429f91069551b9799
Aleksey Kostomarov <kostomarov00@yandex.ru>
2023-04-30 14:24:44 +0300

    Проектная работа №4

---
+++ script.js
@@ -0,0 +1,43 @@
+let popup = document.querySelector('.popup');
+let buttonEdit = document.querySelector('.profile__button-edit');
+let buttonClosePopup = document.querySelector('.popup__close-button');
+
+let popupForm = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__text-field_name');
+let jobInput = document.querySelector('.popup__text-input_job');
+
+let profileUsername = document.querySelector('.profile__username');
+let profileDescription = document.querySelector('.profile__user-description');
+
+
+function openPopup() {
+	popup.classList.add('popup_visible');
+
+	profileDescription.textContent = jobInput.value;
+	profileUsername.textContent = nameInput.value;
+}
+
+function closePopup() {
+	popup.classList.remove('popup_visible')
+}
+
+function handleFormSubmit(evt) {
+	evt.preventDefault();
+
+	profileDescription.textContent = jobInput.value;
+	profileUsername.textContent = nameInput.value;
+
+	closePopup()
+}
+
+buttonEdit.addEventListener('click', openPopup);
+buttonClosePopup.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', handleFormSubmit);
+
+
+
+
+
+
+
+
