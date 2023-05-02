69e77a2b0ad542f42cfcc80ee600c390fe63e0d9
Aleksey Kostomarov <kostomarov00@yandex.ru>
2023-05-02 16:32:59 +0300

    fix: исправлены ошибки первого ревью

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@ let buttonEdit = document.querySelector('.profile__button-edit');
 let buttonClosePopup = document.querySelector('.popup__close-button');
 
 let popupForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__text-input_name');
-let jobInput = document.querySelector('.popup__text-input_job');
+let nameInput = document.querySelector('.popup__text-input_type_name');
+let jobInput = document.querySelector('.popup__text-input_type_job');
 
 let profileUsername = document.querySelector('.profile__username');
 let profileDescription = document.querySelector('.profile__user-description');
@@ -13,8 +13,8 @@ let profileDescription = document.querySelector('.profile__user-description');
 function openPopup() {
 	popup.classList.add('popup_visible');
 
-	profileDescription.textContent = jobInput.value;
-	profileUsername.textContent = nameInput.value;
+	jobInput.value = profileDescription.textContent;
+	nameInput.value = profileUsername.textContent;
 }
 
 function closePopup() {
