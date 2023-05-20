0e56ac0a93e8709fee062d639b5a40a4bad92440
Aleksey Kostomarov <kostomarov00@yandex.ru>
2023-05-20 15:40:43 +0300

    Проектная работа №5

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const popupEdit = document.querySelector('.popup_type_profile');
 const buttonEdit = document.querySelector('.profile__button-edit');
-const buttonClosePopupProfile = document.querySelector('.popup__close-button_type_profile');
+const buttonClosePopupProfile = popupEdit.querySelector('.popup__close-button');
 const popupFormProfile = document.querySelector('.popup__form_type_profile');
 const nameInput = document.querySelector('.popup__text-input_type_name');
 const jobInput = document.querySelector('.popup__text-input_type_job');
@@ -12,20 +12,20 @@ const popupAddCard = document.querySelector('.popup_type_elements');
 const formAddCard = popupAddCard.querySelector('.popup__form_type_elements');
 const formAddCardInputLink = formAddCard.querySelector('.popup__text-input_type_link');
 const formAddCardInputDescription = formAddCard.querySelector('.popup__text-input_type_description');
-const buttonClosePopupAddCards = document.querySelector('.popup__close-button_type_elements');
-const photoCards = document.querySelector('.elements');
-const templateCards = document.querySelector('.elements__template').content;
+const buttonClosePopupAddCards = popupAddCard.querySelector('.popup__close-button');
+const photoCard = document.querySelector('.elements');
+const templateCard = document.querySelector('.elements__template').content;
 
 const popupImage = document.querySelector('.popup_type_image');
 const popupImageContainer = popupImage.querySelector('.popup__container_type_image')
-const buttonClosePopupImage = popupImage.querySelector('.popup__close-button_type_image');
+const buttonClosePopupImage = popupImage.querySelector('.popup__close-button');
 const popupImagePhoto = popupImage.querySelector('.popup__image-full-size');
 const popupImageTitle = popupImage.querySelector('.popup__image-title');
 
 
 
 
-const delitePhoto = (evt) => {
+const deletePhoto = (evt) => {
 	const item = evt.target.closest('.elements__container');
 	item.remove();
 }
@@ -37,8 +37,7 @@ const handleFormSubmitAddCard = (evt) => {
 		name: formAddCardInputDescription.value
 	}
 	renderElements(inputValue);
-	formAddCardInputLink.value = "";
-	formAddCardInputDescription.value = "";
+	formAddCard.reset();
 
 	closePopup(popupAddCard)
 }
@@ -53,23 +52,28 @@ const handleFormSubmitProfile = (evt) => {
 }
 
 const renderElements = (todo) => {
-	const elementCard = templateCards.cloneNode(true);
-	elementCard.querySelector('.elements__image').src = todo.link;
+	const elementCard = templateCard.cloneNode(true);
+	const elementImage = elementCard.querySelector('.elements__image');
+	elementImage.src = todo.link;
 	elementCard.querySelector('.elements__title').textContent = todo.name;
-	elementCard.querySelector('.elements__image').alt = todo.name;
+	elementImage.alt = todo.name;
 	elementCard.querySelector('.elements__like').addEventListener('click', function (evt) {
 		evt.target.classList.toggle('elements__like_active');
 	})
-	elementCard.querySelector('.elements__delite-button').addEventListener('click', delitePhoto);
+	elementCard.querySelector('.elements__delite-button').addEventListener('click', deletePhoto);
 
-	elementCard.querySelector('.elements__image').addEventListener('click', function (evt) {
+	elementImage.addEventListener('click', function (evt) {
 		showPopup(popupImage);
 		popupImagePhoto.src = todo.link;
 		popupImageTitle.textContent = todo.name;
+		popupImagePhoto.alt = todo.name;
 	})
 
-	photoCards.prepend(elementCard)
+
+	photoCard.prepend(elementCard);
+	return elementCard;
 }
+
 
 initialCards.forEach(function (todo) {
 	renderElements(todo);
@@ -85,6 +89,9 @@ const closePopup = (popUp) => {
 
 buttonEdit.addEventListener('click', function () {
 	showPopup(popupEdit)
+
+	profileUsername.textContent = nameInput.value;
+	profileDescription = jobInput.value;
 });
 buttonAddCard.addEventListener('click', function () {
 	showPopup(popupAddCard)
